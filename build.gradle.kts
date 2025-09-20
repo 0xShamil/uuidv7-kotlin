c33f98845b3c45960f7d0264a514faafb7130b15
0xShamil <unibuffer@tuta.com>
2025-09-20 11:26:28 +0200

    kotlin version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.2.20"
 }
 
 group = "fyi.shamil"
@@ -17,5 +17,5 @@ tasks.test {
     useJUnitPlatform()
 }
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(24)
 }
